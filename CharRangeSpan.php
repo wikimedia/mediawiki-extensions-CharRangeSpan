27cdee708b31ebd1b046c318a4842efd7e38002a
Siebrand Mazeland <siebrand@kitano.nl>
2014-03-30 23:24:32 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ie496bd8b0ac5d4ea495de0eb1c120f9641e2dcfe

--- CharRangeSpan.php
+++ CharRangeSpan.php
@@ -8,7 +8,7 @@ if( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['parserhook'][] = array(
 	'path' => __FILE__,
 	'name' => 'Character range span',
-	'version' => '0.9',
+	'version' => '0.10.0',
 	'author' => 'Nick White',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:CharRangeSpan',
 	'descriptionmsg' => 'charrangespan-desc',
@@ -29,6 +29,7 @@ $wgCharRangeSpanSettings = array(
 );
 
 $wgAutoloadClasses['CharRangeSpan'] = dirname( __FILE__ ) . '/CharRangeSpan.body.php';
+$wgMessagesDirs['CharRangeSpan'] = __DIR__ . '/i18n';
 $wgExtensionMessagesFiles['CharRangeSpan'] = dirname( __FILE__ ) . '/CharRangeSpan.i18n.php';
 $wgParserTestFiles[] = dirname( __FILE__ ) . '/tests/parserTests.txt';
 
